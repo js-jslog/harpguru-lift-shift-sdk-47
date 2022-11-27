aee09cfbd84c6dc1c8ff726f74200032f2498df4
js-jslog <jhs4jbs@hotmail.co.uk>
2022-11-27 21:39:49 +0000

    Create simple animated page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, Image } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
 
-export default function App() {
+export default () => {
+  const randomNumber = useSharedValue(100);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withSpring(randomNumber.value),
+      height: withSpring(randomNumber.value, { stiffness: 10 }),
+    };
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        backgroundColor: '#7CA1B4',
+      }}
+    >
+      <TouchableOpacity
+        onPress={() => {
+          randomNumber.value = Math.random() * 350;
+        }}
+      >
+        <Animated.Image
+          source={require('./assets/icon.png')}
+          resizeMode="contain"
+          style={style}
+        />
+      </TouchableOpacity>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
