bb10fd831ffa3b5e85179d47755a0e4f098dd440
js-jslog <jhs4jbs@hotmail.co.uk>
2022-12-04 22:19:18 +0000

    Improve use of reanimated components in harpguru component
    Similar to previous 3 commits.

--- packages/harpguru-core/src/components/harp-guru/harp-guru.tsx
+++ packages/harpguru-core/src/components/harp-guru/harp-guru.tsx
@@ -20,21 +20,22 @@ export const HarpGuru = (): ReactElement => {
   useWindowDimensions()
 
   const { shortEdge: offScreen } = getWindowDimensions()
+
   const pageOnDisplay = useSharedValue<PageNumber>(1)
-  const pageOnDisplayTiming = withTiming(pageOnDisplay.value, {
-    duration: 300,
-    easing: Easing.inOut(Easing.ease)
-  })
-  // The strange decimal stopoff on the way to the next integer
-  // in each of these is to allow the animation to appear to
-  // take the page just barely out of frame, but to then take
-  // it a great distance away at the end. This means the animation
-  // is as smooth as possible, but the page is well out of the
-  // way in case any animated objects move outside of it's page
-  // frame towards interferring with the next page.
-  const page1Y = interpolate(pageOnDisplayTiming, [1, 1.9, 2], [0, offScreen, offScreen * 10])
   const page1YAnimationStyle = useAnimatedStyle(() => {
-    return { transform: [{ translateY: page1Y }]}
+    const timingValue = withTiming(pageOnDisplay.value, {
+      duration: 300,
+      easing: Easing.inOut(Easing.ease)
+    })
+    // The strange decimal stopoff on the way to the next integer
+    // in each of these is to allow the animation to appear to
+    // take the page just barely out of frame, but to then take
+    // it a great distance away at the end. This means the animation
+    // is as smooth as possible, but the page is well out of the
+    // way in case any animated objects move outside of it's page
+    // frame towards interferring with the next page.
+    const translateY = interpolate(timingValue, [1, 1.9, 2], [0, offScreen, offScreen * 10])
+    return { transform: [{ translateY }]}
   })
 
   return (
