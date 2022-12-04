d0c99fab7c06c440a6d1fc52f6a6d963e3782441
js-jslog <jhs4jbs@hotmail.co.uk>
2022-12-04 22:17:46 +0000

    Improve the use of reanimated components in harp cells
    Similar to previous 2 commits.

--- packages/harpguru-core/src/components/harp-cell-accessible/hooks/use-tap-animation-value/use-tap-animation-value.ts
+++ packages/harpguru-core/src/components/harp-cell-accessible/hooks/use-tap-animation-value/use-tap-animation-value.ts
@@ -1,17 +1,16 @@
-import { Easing, interpolate, withTiming, useDerivedValue, useSharedValue  } from 'react-native-reanimated'
+import { Easing, interpolate, withTiming, useDerivedValue } from 'react-native-reanimated'
+import type { SharedValue } from 'react-native-reanimated'
 
 import { tapAnimationDuration } from '../../../../constants'
 
-export const useTapAnimationValue = (isTapped: boolean): number => {
-  const sharedValue = useSharedValue(isTapped)
-  const derivedValue = useDerivedValue(() => {
-    return (sharedValue ? 1 : 0)
+export const useTapAnimationValue = (isTapped: boolean): SharedValue<number> => {
+  const animationValue = useDerivedValue(() => {
+    const timingValue = withTiming((isTapped ? 1 : 0), {
+      duration: tapAnimationDuration,
+      easing: Easing.inOut(Easing.circle)
+    })
+    return interpolate(timingValue, [0, 1], isTapped ? [0.5, 1] : [1, 1])
   })
-  const timingValue = withTiming(derivedValue.value, {
-    duration: tapAnimationDuration,
-    easing: Easing.inOut(Easing.circle)
-  })
-  const animationValue = interpolate(timingValue, [0, 1], isTapped ? [0.5, 1] : [1, 1])
 
   return animationValue
 }
