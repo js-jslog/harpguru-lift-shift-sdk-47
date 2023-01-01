aa89a77bc89883f2b81e5750242b3dce582e686c
js-jslog <jhs4jbs@hotmail.co.uk>
2023-01-01 20:45:46 +0000

    Remove unnecessary packages

--- apps/harpguru/jest-setup.js
+++
@@ -1,7 +0,0 @@
-require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests()
-
-jest.mock('react-native-reanimated', () =>
-  require('react-native-reanimated/mock')
-)
-
-require('../../node_modules/react-native-gesture-handler/jestSetup.js')
